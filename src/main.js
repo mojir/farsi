f4a36057bbd5a8beba9a0b55f787a11c2b9cfdd0
Albert Mojir <albert.mojir@gmail.com>
2015-12-05 18:33:42 +0100

    Some random initial stuff

---
+++ src/main.js
@@ -0,0 +1,19 @@
+window.onload = function () {
+    table = {
+        A: "آ",
+        B: "
+
+
+
+
+
+
+        //<D-+><D-+>
+
+       //اوباما: آمریکا مرعوب تیراندازی‌ کالیفرنیا نمی‌شود 
+        //
+        };
+
+
+    document.getElementById("h1").innerHTML = "\ud8a2";
+}
